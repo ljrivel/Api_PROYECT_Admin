2e2043d63d22fe5d648b3a5dba03d6d74c8bf5e9
ljrivel <ljrivel@gmail.com>
2022-02-15 14:38:37 -0600

    fix to config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     database: {
-        host: process.env.DB_HOST || "localhost",
-        user: process.env.DB_USER || "root",
-        password: process.env.DB_PASSWORD || "root",
-        database: process.env.DB_DATABASE || "cinepolis"
+        host: process.env.DBHOST || "localhost",
+        user: process.env.DBUSER || "root",
+        password: process.env.DBPASSWORD || "root",
+        database: process.env.DBDATABASE || "cinepolis"
     }
 
 }
