92e951c7b8a88dbf2e44c7e67580c94c3309fafb
ljrivel <ljrivel@gmail.com>
2022-02-15 15:43:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -18,16 +18,6 @@ const corsOptions ={
 
 app.use(cors(corsOptions))
 
-
-
-connection.connect((err) =>{
-if (err) throw err;
-console.log("Connected to database");
-});
-
-
-
-
 app.use(express.json());
 
 
