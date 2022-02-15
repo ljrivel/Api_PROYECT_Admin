75b37576e16cebd24f3d035e5fdd76f581ab6209
ljrivel <ljrivel@gmail.com>
2022-02-15 14:48:39 -0600

    change config information

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     database: {
-        host: process.env.DBHOST || "localhost",
-        user: process.env.DBUSER || "root",
-        password: process.env.DBPASSWORD || "root",
-        database: process.env.DBDATABASE || "cinepolis"
+        host: process.env.DBHOST || "us-cdbr-east-05.cleardb.net",
+        user: process.env.DBUSER || "b7af72ef51bc8c",
+        password: process.env.DBPASSWORD || "bb6d67f2",
+        database: process.env.DBDATABASE || "heroku_d23e98632d70552"
     }
 
 }
