2eaecbdd6a2dfe80d5b156dbf507ac7bff902ab5
ljrivel <ljrivel@gmail.com>
2022-03-11 18:38:54 -0600

    try to fix 1

--- app.js
+++ app.js
@@ -104,6 +104,7 @@ app.post("/Login", (req, res) => {
 
  app.get("/GetPelicula", (req, res) => {
   const body = req.body
+  console.log(body);
   GetPelicula(connection,body,result =>{
     res.json(result);
   });
