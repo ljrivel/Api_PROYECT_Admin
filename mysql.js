f2d4591391ec1d44d05874e62b3af01945e63517
ljrivel <ljrivel@gmail.com>
2022-03-16 09:39:39 -0600

    Cambio mysql.js

--- mysql.js
+++ mysql.js
@@ -2,7 +2,7 @@ const { database } = require('./config');
 const mysql = require('mysql');
 
 // Crear la conexion
-const connection = mysql.createConnection(database);
+const connection = mysql.createPool(database);
    
 // Conectamos a la base de datos
 connection.connect();
