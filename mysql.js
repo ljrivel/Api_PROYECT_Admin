7ae3058dd17e4b1ea07bc704ec36bc0968d2b4be
ljrivel <ljrivel@gmail.com>
2022-03-16 09:44:10 -0600

    backup

--- mysql.js
+++ mysql.js
@@ -2,9 +2,9 @@ const { database } = require('./config');
 const mysql = require('mysql');
 
 // Crear la conexion
-const connection = mysql.createPool(database);
+const connection = mysql.createConnection(database);
    
 // Conectamos a la base de datos
-connection.createPool();
+connection.connect();
 
 module.exports = connection;
