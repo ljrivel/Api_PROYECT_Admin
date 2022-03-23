b1f207345f0d7fe98c972e8044acb9a91a70090a
ljrivel <ljrivel@gmail.com>
2022-03-23 15:39:21 -0600

    change GetUserLogin

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/Login", (req, res) => {
 
 
  app.post("/GetUserLogin", (req, res) => {
-  const body = {Usuario : 'l@g'}
+  const body = req.body
   getUserLogin(connection,body,result =>{
     res.json(result);
   });
