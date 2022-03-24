e0969133c7c8f4ae7cf8d1a9de8e7ac6a34539a3
ljrivel <ljrivel@gmail.com>
2022-03-23 18:48:50 -0600

    error with insert pelicula

--- operations.js
+++ operations.js
@@ -114,10 +114,11 @@ function changeUser(connection,data,callback){
 //-----------------------------|
 
 function InsertPelicula(connection,data,callback){
+    console.log(data);
     let insertQuery =
         "INSERT INTO Pelicula (Titulo,Director,YearPublicacion,EdadRequeridad,URL) VALUES (?,?,?,?,?)"
         let queryPeli = mysql.format(insertQuery,[data.Titulo,data.Director,data.Year,data.Edad,data.URL]);
-
+        console.log(queryPeli);
         connection.query(queryPeli,function(err,result){
             if(err) throw err;
             callback(result);
