110c5b2232ae1636b0cc2f6d3e340c81183c45af
ljrivel <ljrivel@gmail.com>
2022-06-01 11:58:02 -0600

    Always on

--- mysql.js
+++ mysql.js
@@ -1,10 +1,13 @@
 const { database } = require('./config');
 const mysql = require('mysql');
 
-// Crear la conexion
-const connection = mysql.createConnection(database);
-   
+
 // Conectamos a la base de datos
-connection.connect();
+var connection = mysql.createPool({
+    host: process.env.DBHOST || "us-cdbr-east-05.cleardb.net",
+    user: process.env.DBUSER || "b7af72ef51bc8c",
+    password: process.env.DBPASSWORD || "bb6d67f2",
+    database: process.env.DBDATABASE || "heroku_d23e98632d70552"
+  });
 
 module.exports = connection;
